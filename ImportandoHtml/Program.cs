cd8f889570e03d2724ddba08050445e4e6b47c48
luisgustavogomes <luisgustavogomes@outlook.com>
2019-07-22 20:29:52 -0300

    Adicionar arquivos de projeto.

---
+++ ImportandoHtml/Program.cs
@@ -0,0 +1,20 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportandoHtml
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(@"Files\HTMLPage1.html");
+            foreach (HtmlNode div in doc.DocumentNode.SelectNodes("//div"))
+                    Console.WriteLine(div.InnerText);
+        }
+    }
+}
